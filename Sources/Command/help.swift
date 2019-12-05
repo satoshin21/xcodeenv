edc767443d720d39945b3a63c2479da350eabd89
Satoshi Nagasaka <satoshi.nagasaka21@gmail.com>
2019-12-06 01:50:33 +0900

    add subcommands

---
+++ Sources/Command/help.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Satoshi Nagasaka on 2019/12/06.
+//
+
+import Foundation
+
+public func help() {
+
+}
--- Sources/xcodeenv/main.swift
+++ Sources/xcodeenv/main.swift
@@ -1,2 +1,37 @@
-print("Hello, world!")
+import SPMUtility
+import Swift
+import Basic
+import Foundation
 
+func main(_ args: [String]) {
+
+    let parser = ArgumentParser(
+        commandName: "xcodeenv",
+        usage: "<command> [<args>]",
+        overview: "xcode select management tool")
+
+    let local = parser.add(subparser: "local", usage: "[<version name>]", overview: "Set or show the local application-specific Xcode version")
+    let versions = parser.add(subparser: "versions", overview: "List all Xcode versions")
+    let version = parser.add(subparser: "version", overview: "Show the current Xcode version and its origin")
+
+    do {
+
+        let res = try parser.parse(args)
+
+        guard !args.isEmpty else {
+            parser.printUsage(on: stdoutStream)
+            exit(0)
+        }
+
+        let subparser = res.subparser(local)
+
+        print("subparser res: " + subparser ?? "")
+
+
+
+    } catch let e {
+        print("error!\(e)")
+    }
+}
+
+main(Array(CommandLine.arguments.dropFirst()))
