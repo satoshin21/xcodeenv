f92344a7573f938eb060efcaa5449c44c87d702b
Satoshi Nagasaka <satoshi.nagasaka21@gmail.com>
2019-12-11 20:21:53 +0900

    add locaversion_name subcommand

--- Sources/xcodeenv/main.swift
+++ Sources/xcodeenv/main.swift
@@ -1,7 +1,7 @@
-import SPMUtility
 import Swift
 import Basic
 import Foundation
+import SPMUtility
 
 func main(_ args: [String]) {
 
@@ -11,6 +11,8 @@ func main(_ args: [String]) {
         overview: "xcode select management tool")
 
     let local = parser.add(subparser: "local", usage: "[<version name>]", overview: "Set or show the local application-specific Xcode version")
+    let local_versionName = local.add(positional: "version name", kind: String.self, optional: true, usage: "[<version name>]", completion: ShellCompletion.none)
+
     let versions = parser.add(subparser: "versions", overview: "List all Xcode versions")
     let version = parser.add(subparser: "version", overview: "Show the current Xcode version and its origin")
 
@@ -23,14 +25,35 @@ func main(_ args: [String]) {
             exit(0)
         }
 
-        let subparser = res.subparser(local)
+        if try res.get("local") != nil {
+            stdoutStream <<< "is_local"
+            stdoutStream.flush()
+        }
+        
 
-        print("subparser res: " + subparser ?? "")
+        if let localParser = res.subparser(local) {
+            print("localparser: \(localParser)")
+        }
 
+        if let versionName = res.get(local_versionName) {
+            print("versionName: \(versionName)")
+        }
 
+    } catch let e as ArgumentParserError {
 
+        switch e {
+        case .expectedArguments(let parser, let arguments):
+            print(error: e)
+            parser.printUsage(on: stdoutStream)
+        default:
+            break
+        }
+
+        exit(1)
     } catch let e {
-        print("error!\(e)")
+
+        parser.printUsage(on: stdoutStream)
+        exit(1)
     }
 }
 
